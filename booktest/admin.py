3ab4663af2cb663f07c1ad0180d347bf14510be2
szk_django04 <szk_django04@163.com>
2018-11-07 23:42:49 +0800

    5常用——admin站点——操作选项位置

--- booktest/admin.py
+++ booktest/admin.py
@@ -4,14 +4,16 @@ from booktest.models import *
 # Register your models here.
 
 
+class AreaAdmin(admin.ModelAdmin):
+    list_per_page = 10
+    actions_on_top = True
+    actions_on_bottom = True
+
+
+admin.site.register(AreaInfo, AreaAdmin)
+# admin.site.register(AreaInfo)
+#
+#
+# @admin.register(AreaInfo)
 # class AreaAdmin(admin.ModelAdmin):
 #     pass
-#
-#
-# admin.site.register(AreaInfo, AreaAdmin)
-admin.site.register(AreaInfo)
-
-
-@admin.register(AreaInfo)
-class AreaAdmin(admin.ModelAdmin):
-    pass
