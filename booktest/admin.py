5ed3a0f072faedc5f37fda41d0906cc3cdf391d4
szk_django04 <szk_django04@163.com>
2018-11-07 21:56:59 +0800

    5常用——控制管理页展示

--- booktest/admin.py
+++ booktest/admin.py
@@ -2,4 +2,16 @@ from django.contrib import admin
 from booktest.models import *
 
 # Register your models here.
+
+
+# class AreaAdmin(admin.ModelAdmin):
+#     pass
+#
+#
+# admin.site.register(AreaInfo, AreaAdmin)
 admin.site.register(AreaInfo)
+
+
+@admin.register(AreaInfo)
+class AreaAdmin(admin.ModelAdmin):
+    pass
