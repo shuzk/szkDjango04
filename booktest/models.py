d3ae949428352c857849b061e63ac50e302938ca
szk_django04 <szk_django04@163.com>
2018-11-07 23:58:46 +0800

    5常用——admin站点——中文标题

--- booktest/models.py
+++ booktest/models.py
@@ -3,7 +3,8 @@ from django.db import models
 
 # 定义地区模型类，存储省、市、区县信息
 class AreaInfo(models.Model):
-    atitle = models.CharField(max_length=30)  # 名称
+    atitle = models.CharField("标题", max_length=30)
+    # atitle = models.CharField(max_length=30)  # 名称
     aParent = models.ForeignKey('self', null=True, blank=True)  # 父级
 
     # 将方法作为列
