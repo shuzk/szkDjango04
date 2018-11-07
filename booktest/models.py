b2eb10554075dfc2ace98443ae4032f1d16c84d6
szk_django04 <szk_django04@163.com>
2018-11-07 19:59:33 +0800

    5常用——安装booktest应用，配置数据库szkDjango022,创建模板文件并配置模板查找路径，配置urls包含booktest的配置，配置booktest的urls，定义视图index，创建index.html，创建模型类AreaInfo

--- booktest/models.py
+++ booktest/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+# 定义地区模型类，存储省、市、区县信息
+class AreaInfo(models.Model):
+    atitle = models.CharField(max_length=30)  # 名称
+    aParent = models.ForeignKey('self', null=True, blank=True)  # 父级
--- szk_django04/settings.py
+++ szk_django04/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'booktest',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'szk_django04.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,9 +76,17 @@ WSGI_APPLICATION = 'szk_django04.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "szkDjango022",
+        "HOST": "localhost",
+        "PORT": 3306,
+        "USER": "root",
+        "PASSWORD": "mysql",
     }
 }
 
